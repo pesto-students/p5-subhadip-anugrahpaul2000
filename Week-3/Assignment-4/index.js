689e1879df23cd08ee61aeb789dbc04cbffa6bb1
Anugrah Vishwas Paul <anugrahpaul2000@gmail.com>
2022-09-01 23:55:48 +0530

    Assignments Done

--- Week-3/Assignment-4/index.js
+++ Week-3/Assignment-4/index.js
@@ -1,18 +1,23 @@
 function createStack() {
+    const items = [];
     return {
-        items: [],
         push(item) {
-            this.items.push(item);
+            items.push(item);
         },
         pop() {
-            return this.items.pop();
+            return items.pop();
+        },
+        status() {
+            console.log(items);
         }
     };
 }
-
+  
 const stack = createStack();
 stack.push(10);
 stack.push(5);
+console.log(stack.status());
 stack.pop();
-stack.items;
+console.log(stack.status());
 stack.items = [10, 100, 1000];
+console.log(stack.status());
