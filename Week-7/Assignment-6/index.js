67a51fcc0fba01a99a27e87cf5d1025070fb6d72
AVP-cedcoss <anugrahvishwaspaul@cedcoss.com>
2022-10-10 22:31:56 +0530

    Week-7 done

--- Week-7/Assignment-6/index.js
+++ Week-7/Assignment-6/index.js
@@ -1,27 +1,53 @@
-const Queue = (input) => {
-  let queue = [];
-  let result = [];
-  let i = 0;
-  while (i < input.length) {
-    switch (input[i]) {
-      case 1:
-        queue.push(input[i + 1]);
-        i += 2;
-        break;
-
-      case 2:
-        if (queue.length) {
-            result.push(queue.shift());
-        } else {
-            result.push(-1);
-        }
-        i++;
-        break;
-    }
+"use strict";
+class Queue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
   }
 
-  return result;
-};
+  enqueue(element) {
+    while (this.stack1.length != 0) {
+      this.stack2.push(this.stack1.pop());
+    }
 
-console.log(Queue([1, 2, 1, 3, 2, 1, 4, 2]));
-console.log(Queue([1, 2, 2, 2, 1, 4]));
+    this.stack1.push(element);
+
+    while (this.stack2.length != 0) {
+      this.stack1.push(this.stack2.pop());
+    }
+    return element;
+  }
+
+  dequeue() {
+    if (this.stack1.length == 0) {
+      return -1;
+    }
+
+    let element = this.stack1[this.stack1.length - 1];
+    this.stack1.pop();
+    return element;
+  }
+
+  operation(input) {
+    let result = [];
+    let i = 0;
+    while (i < input.length) {
+      switch (input[i]) {
+        case 1:
+          this.enqueue(input[i + 1]);
+          i += 2;
+          break;
+
+        case 2:
+          result.push(this.dequeue());
+          i++;
+          break;
+      }
+    }
+    return result;
+  }
+}
+
+const Q = new Queue();
+console.log(Q.operation([1, 2, 1, 3, 2, 1, 4, 2]));
+console.log(Q.operation([1, 2, 2, 2, 1, 4]));
